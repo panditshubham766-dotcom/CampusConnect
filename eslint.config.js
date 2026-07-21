d010e4437a0bfcaa0ba76a8cebb01dd86eeae5bc
Milan <milanrathod5201@gmail.com>
2026-07-21 21:37:56 +0530

    chore: fix eslint ignores for wasm pkg and update package-lock

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist", ".output", ".vinxi", "supabase/functions"] },
+  { ignores: ["dist", ".output", ".vinxi", "supabase/functions", "wasm/image-compressor/pkg"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
